7b6b5a2c8d99fde5cbd2d607eece887a819d6f10
jenirg <38708051+jenirg@users.noreply.github.com>
2018-11-18 12:53:26 -0600

    Tercer-Proyecto.rb
    modificacion

--- Tercer-Proyecto.rb
+++ Tercer-Proyecto.rb
@@ -214,6 +214,370 @@ def ordenar_lista(lista, a)
 
 
 end
+
+def ver(cola)
+    limpiar
+    
+    as = cola[:tope]
+    while as[:siguiente] != nil
+    puts as[:valor]
+    as=as[:siguiente]     
+    end
+    puts as[:valor]
+    puts cola[:tope]
+end
+def ver2(cola2)
+    limpiar
+    
+    as = cola2[:tope]
+    while as[:siguiente] != nil
+    puts as[:valor]
+    as=as[:siguiente]     
+    end
+    puts as[:valor]
+    puts cola2[:tope]
+end
+def ver3(cola3)
+    limpiar
+    
+    as = cola3[:tope]
+    while as[:siguiente] != nil
+    puts as[:valor]
+    as=as[:siguiente]     
+    end
+    puts as[:valor]
+    puts cola3[:tope]
+end
+def ver4(cola4)
+    limpiar
+    
+    as = cola4[:tope]
+    while as[:siguiente] != nil
+    puts as[:valor]
+    as=as[:siguiente]     
+    end
+    puts as[:valor]
+    puts cola4[:tope]
+end
+
+def ordena(cola, cola2, cola3, cola4)
+  limpiar
+    puts 'Arreglo a ordenar'
+    puts 'Forma Asendente'
+    puts cola[:tope][:valor]
+    puts  cola2[:tope][:valor] 
+    puts cola3[:tope][:valor] 
+    puts cola4[:tope][:valor]
+    puts '______________________________'
+    if cola[:tope][:valor] > cola2[:tope][:valor] && cola[:tope][:valor] > cola3[:tope][:valor] && cola[:tope][:valor] > cola4[:tope][:valor]
+  
+      if  cola[:tope][:valor] > cola2[:tope][:valor] && cola[:tope][:valor] > cola3[:tope][:valor] && cola[:tope][:valor] > cola4[:tope][:valor]
+        puts cola[:tope][:valor]
+      end
+    
+      if  cola2[:tope][:valor] > cola[:tope][:valor] && cola2[:tope][:valor] > cola3[:tope][:valor] && cola2[:tope][:valor] > cola4[:tope][:valor]
+        puts cola2[:tope][:valor]
+      end
+    
+      if  cola3[:tope][:valor] > cola[:tope][:valor] && cola3[:tope][:valor] > cola2[:tope][:valor] && cola3[:tope][:valor] > cola4[:tope][:valor]
+        puts cola3[:tope][:valor]
+      end 
+      if  cola4[:tope][:valor] > cola[:tope][:valor] && cola4[:tope][:valor] > cola2[:tope][:valor] && cola4[:tope][:valor] > cola3[:tope][:valor]
+        puts cola4[:tope][:valor]
+      end 
+      puts '______________________________' 
+      if  cola2[:tope][:valor] > cola3[:tope][:valor] && cola2[:tope][:valor] > cola4[:tope][:valor] 
+        puts cola2[:tope][:valor]
+      end
+      if  cola3[:tope][:valor] > cola2[:tope][:valor] && cola3[:tope][:valor] > cola4[:tope][:valor] 
+        puts cola3[:tope][:valor]
+      end
+      if  cola4[:tope][:valor] > cola2[:tope][:valor] && cola4[:tope][:valor] > cola3[:tope][:valor] 
+        puts cola4[:tope][:valor]
+      end
+      puts '______________________________' 
+      if  cola2[:tope][:valor] > cola3[:tope][:valor] 
+        puts cola2[:tope][:valor]
+      end
+      
+      if  cola3[:tope][:valor] > cola2[:tope][:valor] 
+        puts cola3[:tope][:valor]
+      end
+      puts '______________________________' 
+      puts cola2[:tope][:valor]
+      puts '______________________________' 
+    end 
+    if cola2[:tope][:valor] > cola[:tope][:valor] && cola2[:tope][:valor] > cola3[:tope][:valor] && cola2[:tope][:valor] > cola4[:tope][:valor]
+        if  cola2[:tope][:valor] > cola[:tope][:valor] && cola2[:tope][:valor] > cola3[:tope][:valor] && cola2[:tope][:valor] > cola4[:tope][:valor]
+            puts cola2[:tope][:valor]
+          end
+     puts '______________________________' 
+          if  cola4[:tope][:valor] > cola[:tope][:valor] && cola4[:tope][:valor] > cola3[:tope][:valor]
+            puts cola4[:tope][:valor]
+          end
+    puts '______________________________' 
+          if  cola3[:tope][:valor] > cola[:tope][:valor] 
+            puts cola3[:tope][:valor]
+          end 
+     puts '______________________________' 
+     puts cola[:tope][:valor]
+     puts '______________________________' 
+    end 
+    if cola3[:tope][:valor] > cola[:tope][:valor] && cola3[:tope][:valor] > cola2[:tope][:valor] && cola3[:tope][:valor] > cola4[:tope][:valor]
+        if  cola3[:tope][:valor] > cola[:tope][:valor] && cola3[:tope][:valor] > cola2[:tope][:valor] && cola3[:tope][:valor] > cola4[:tope][:valor]
+            puts cola3[:tope][:valor]
+        end
+     puts '______________________________'
+     if  cola4[:tope][:valor] > cola[:tope][:valor] && cola4[:tope][:valor] > cola2[:tope][:valor]
+        puts cola4[:tope][:valor]
+      end
+     puts '______________________________' 
+     if  cola[:tope][:valor] > cola2[:tope][:valor] 
+        puts cola[:tope][:valor]
+      end 
+      puts '______________________________' 
+      puts cola2[:tope][:valor]
+      puts '______________________________' 
+
+    end 
+    if cola4[:tope][:valor] > cola[:tope][:valor] && cola4[:tope][:valor] > cola2[:tope][:valor] && cola4[:tope][:valor] > cola3[:tope][:valor]
+        if  cola4[:tope][:valor] > cola[:tope][:valor] && cola4[:tope][:valor] > cola2[:tope][:valor] && cola4[:tope][:valor] > cola3[:tope][:valor]
+            puts cola4[:tope][:valor]
+        end
+     puts '______________________________' 
+     if  cola[:tope][:valor] > cola2[:tope][:valor] && cola[:tope][:valor] > cola3[:tope][:valor]
+        puts cola[:tope][:valor]
+      end
+     puts '______________________________' 
+    
+    if  cola3[:tope][:valor] > cola2[:tope][:valor] 
+        puts cola3[:tope][:valor]
+      end 
+      puts '______________________________' 
+      puts cola2[:tope][:valor]
+      puts '______________________________' 
+
+    end 
+    
+end 
+def ordenad(cola, cola2, cola3, cola4)
+  limpiar
+    puts 'Forma Desendente'
+    puts 'Arreglo a ordenar'
+    
+    puts cola[:tope][:valor]
+    puts  cola2[:tope][:valor] 
+    puts cola3[:tope][:valor] 
+    puts cola4[:tope][:valor]
+    puts '______________________________'
+    if cola2[:tope][:valor] < cola[:tope][:valor] && cola2[:tope][:valor] < cola3[:tope][:valor] && cola2[:tope][:valor] < cola4[:tope][:valor]
+        if  cola2[:tope][:valor] < cola3[:tope][:valor] && cola2[:tope][:valor] < cola4[:tope][:valor] && cola2[:tope][:valor] < cola4[:tope][:valor]
+            puts cola2[:tope][:valor]
+          end
+          puts '______________________________' 
+          if  cola3[:tope][:valor] < cola[:tope][:valor] && cola3[:tope][:valor] < cola4[:tope][:valor] 
+            puts cola3[:tope][:valor]
+          end
+          puts '______________________________' 
+          if  cola4[:tope][:valor] < cola[:tope][:valor] 
+            puts cola4[:tope][:valor]
+          end 
+          puts '______________________________' 
+          puts cola[:tope][:valor]
+          puts '______________________________' 
+    end 
+    if cola[:tope][:valor] < cola2[:tope][:valor] && cola[:tope][:valor] < cola3[:tope][:valor] && cola[:tope][:valor] < cola4[:tope][:valor]
+        if  cola[:tope][:valor] < cola2[:tope][:valor] && cola[:tope][:valor] < cola3[:tope][:valor] && cola[:tope][:valor] < cola4[:tope][:valor]
+            puts cola[:tope][:valor]
+          end
+     puts '______________________________' 
+     if  cola3[:tope][:valor] < cola2[:tope][:valor] && cola3[:tope][:valor] < cola4[:tope][:valor]
+        puts cola3[:tope][:valor]
+      end
+ puts '______________________________' 
+ if  cola4[:tope][:valor] < cola2[:tope][:valor] 
+    puts cola4[:tope][:valor]
+  end
+puts '______________________________' 
+puts cola2[:tope][:valor]
+ puts '______________________________' 
+
+    end 
+    if cola2[:tope][:valor] < cola[:tope][:valor] && cola2[:tope][:valor] < cola3[:tope][:valor] && cola2[:tope][:valor] < cola4[:tope][:valor]
+        if  cola2[:tope][:valor] < cola[:tope][:valor] && cola2[:tope][:valor] < cola3[:tope][:valor] && cola2[:tope][:valor] < cola4[:tope][:valor]
+            puts cola2[:tope][:valor]
+        end
+     puts '______________________________' 
+     if  cola[:tope][:valor] < cola3[:tope][:valor] && cola[:tope][:valor] < cola4[:tope][:valor]
+        puts cola[:tope][:valor]
+      end
+     puts '______________________________' 
+     if  cola4[:tope][:valor] < cola3[:tope][:valor] 
+        puts cola4[:tope][:valor]
+      end 
+      puts '______________________________' 
+      puts cola3[:tope][:valor]
+      puts '______________________________' 
+    end 
+    if cola2[:tope][:valor] < cola[:tope][:valor] && cola2[:tope][:valor] < cola3[:tope][:valor] && cola2[:tope][:valor] < cola4[:tope][:valor]
+        if  cola2[:tope][:valor] < cola[:tope][:valor] && cola2[:tope][:valor] < cola3[:tope][:valor] && cola2[:tope][:valor] < cola4[:tope][:valor]
+            puts cola2[:tope][:valor]
+        end
+     puts '______________________________' 
+     if  cola3[:tope][:valor] < cola[:tope][:valor] && cola3[:tope][:valor] < cola4[:tope][:valor]
+        puts cola3[:tope][:valor]
+      end
+      puts '______________________________' 
+      if  cola[:tope][:valor] < cola4[:tope][:valor] 
+        puts cola[:tope][:valor]
+      end 
+      puts '______________________________' 
+      puts cola4[:tope][:valor]
+      puts '______________________________' 
+    end 
+   
+
+end 
+def insertar (cola)
+    limpiar
+
+    print 'Inserte Un Numero: '
+    n = gets.chomp.to_i
+    elemento = {
+        valor: n,
+        siguiente: nil
+    }
+
+    if cola[:esta_vacia] == true
+        cola[:tope] = elemento
+        cola[:fondo] = elemento
+        cola[:esta_vacia] = false
+        cola[:tamaño] = cola[:tamaño] +1 
+    else 
+
+        aux = cola[:fondo]
+        aux[:siguiente] = elemento
+        elemento[:siguiente] = nil
+        
+        cola[:fondo] = elemento
+        cola[:tamaño] = cola[:tamaño] +1
+    end
+
+
+end
+def insertar2 (cola2)
+    limpiar
+
+    print 'Inserte Un Numero: '
+    n = gets.chomp.to_i
+    elemento = {
+        valor: n,
+        siguiente: nil
+    }
+
+    if cola2[:esta_vacia] == true
+        cola2[:tope] = elemento
+        cola2[:fondo] = elemento
+        cola2[:esta_vacia] = false
+        cola2[:tamaño] = cola2[:tamaño] +1 
+    else 
+
+        aux = cola2[:fondo]
+        aux[:siguiente] = elemento
+        elemento[:siguiente] = nil
+        
+        cola2[:fondo] = elemento
+        cola2[:tamaño] = cola2[:tamaño] +1
+    end
+
+
+end
+def insertar3 (cola3)
+    limpiar
+
+    print 'Inserte Un Numero: '
+    n = gets.chomp.to_i
+    elemento = {
+        valor: n,
+        siguiente: nil
+    }
+
+    if cola3[:esta_vacia] == true
+        cola3[:tope] = elemento
+        cola3[:fondo] = elemento
+        cola3[:esta_vacia] = false
+        cola3[:tamaño] = cola3[:tamaño] +1 
+    else 
+
+        aux = cola3[:fondo]
+        aux[:siguiente] = elemento
+        elemento[:siguiente] = nil
+        
+        cola3[:fondo] = elemento
+        cola3[:tamaño] = cola2[:tamaño] +1
+    end
+
+
+end
+def insertar4 (cola4)
+    limpiar
+
+    print 'Inserte Un Numero: '
+    n = gets.chomp.to_i
+    elemento = {
+        valor: n,
+        siguiente: nil
+    }
+
+    if cola4[:esta_vacia] == true
+        cola4[:tope] = elemento
+        cola4[:fondo] = elemento
+        cola4[:esta_vacia] = false
+        cola4[:tamaño] = cola4[:tamaño] +1 
+    else 
+
+        aux = cola4[:fondo]
+        aux[:siguiente] = elemento
+        elemento[:siguiente] = nil
+        
+        cola4[:fondo] = elemento
+        cola4[:tamaño] = cola2[:tamaño] +1
+    end
+
+
+end
+cola = {
+    fondo: nil,
+    tope: nil,
+    max:-1,
+    esta_vacia: true,
+    tamaño:0,
+    esta_llena: false     
+}
+cola2 = {
+    fondo: nil,
+    tope: nil,
+    max:-1,
+    esta_vacia: true,
+    tamaño:0,
+    esta_llena: false     
+}
+cola3 = {
+    fondo: nil,
+    tope: nil,
+    max:-1,
+    esta_vacia: true,
+    tamaño:0,
+    esta_llena: false     
+}
+cola4 = {
+    fondo: nil,
+    tope: nil,
+    max:-1,
+    esta_vacia: true,
+    tamaño:0,
+    esta_llena: false     
+}
     
     
 begin
@@ -242,6 +606,51 @@ begin
             ordenar_pila(a)
         elsif opc == 2
             #ordenar paso a paso cola
+          opcion=0
+while opcion != '10'
+puts 'Listado de Opciones:'
+puts '1.Inserta el elemento1'
+puts '2.Inserta el elemento2'
+puts '3.Inserta el elemento3'
+puts '4.Inserta el elemento4'
+puts '5. Ver primer elemento'
+puts '6. Ver segundo elemento'
+puts '7. Ver tercer elemento'
+puts '8. Ver cuarto  elemento'
+puts '9. Ordenamiento'
+puts '10. Salir'
+print 'Ingrese Opcion: '
+opcion = gets.chomp
+if opcion == '1'
+    insertar(cola)
+end
+if opcion == '2'
+    insertar2(cola2)
+end
+if opcion == '3'
+    insertar3(cola3)
+end 
+if opcion == '4'
+    insertar4(cola4)
+end 
+if opcion == '5'
+    ver(cola)
+end 
+if opcion == '6'
+    ver2(cola2)
+end 
+if opcion == '7'
+    ver3(cola3)
+end 
+if opcion == '8'
+    ver4(cola4)
+end 
+if opcion == '9'
+    ordena(cola, cola2, cola3,cola4)
+    ordenad(cola, cola2,cola3,cola4)
+end 
+end 
+
         elsif opc == 3
             #ordenar paso a paso lista
         else 
